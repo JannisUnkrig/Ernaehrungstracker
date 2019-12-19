850708c525ec7cb2f5a2fe511bb213bb0b98c4d3
Jannis <jannis.unkrig@gmx.de>
2019-12-19 18:16:42 +0100

    more lower design

--- app/src/main/java/com/example/ernaehrungstracker/MainActivity.java
+++ app/src/main/java/com/example/ernaehrungstracker/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -26,16 +27,32 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout = findViewById(R.id.drawer);
         toolbar = findViewById(R.id.toolbar);
         navigationView = findViewById(R.id.navigationView);
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
         toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawerOpen,R.string.drawerClose);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.historie:
+                Toast.makeText(MainActivity.this, "Historie ausgewählt", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.gewichtSpeichern:
+                Toast.makeText(MainActivity.this, "Gewicht speichern ausgewählt", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.gerichteBearbeiten:
+                Toast.makeText(MainActivity.this, "Gerichte bearbeiten ausgewählt", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.einstellungen:
+                Toast.makeText(MainActivity.this, "Einstellungen ausgewählt", Toast.LENGTH_SHORT).show();
+                break;
+        }
         return false;
     }
 }
